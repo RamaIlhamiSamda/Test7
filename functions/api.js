e77b27a3b2a0d03b041558e5a5a01be38f4e8265
RamaIlhamiSamda <ramasamda@gmail.com>
2024-07-08 13:31:55 +0700

    next commit

--- functions/api.js
+++ functions/api.js
@@ -5,37 +5,43 @@ const router = express.Router();
 
 let records = [];
 
-
+//Get all students
 router.get('/', (req, res) => {
   res.send('App is running..');
 });
 
+//Create new record
 router.post('/add', (req, res) => {
   res.send('New record added.');
 });
 
+//delete existing record
 router.delete('/', (req, res) => {
   res.send('Deleted existing record');
 });
 
+//updating existing record
 router.put('/', (req, res) => {
   res.send('Updating existing record');
 });
 
-
+//showing demo records
 router.get('/demo', (req, res) => {
   res.json([
     {
       id: '001',
-      name: 'Aayush',
+      name: 'Smith',
+      email: 'smith@gmail.com',
     },
     {
       id: '002',
-      name: 'rohit',
+      name: 'Sam',
+      email: 'sam@gmail.com',
     },
     {
       id: '003',
-      name: 'Mohit',
+      name: 'lily',
+      email: 'lily@gmail.com',
     },
   ]);
 });
