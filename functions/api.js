d9da2e485db0efe021b33ee27b47b37f1b26ffea
RamaIlhamiSamda <ramasamda@gmail.com>
2024-07-08 11:20:06 +0700

    next commit

--- functions/api.js
+++ functions/api.js
@@ -2,14 +2,43 @@ const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 const router = express.Router();
-const port = 3000
 
-router.get('/toko', (req, res) => {
+let records = [];
+
+
+router.get('/', (req, res) => {
   res.send('App is running..');
 });
 
-router.listening(port,()=>{
-  console.log(`app is listening on port ${port}`)
-})
+router.post('/add', (req, res) => {
+  res.send('New record added.');
+});
+
+router.delete('/', (req, res) => {
+  res.send('Deleted existing record');
+});
+
+router.put('/', (req, res) => {
+  res.send('Updating existing record');
+});
+
+
+router.get('/demo', (req, res) => {
+  res.json([
+    {
+      id: '001',
+      name: 'Aayush',
+    },
+    {
+      id: '002',
+      name: 'rohit',
+    },
+    {
+      id: '003',
+      name: 'Mohit',
+    },
+  ]);
+});
+
 app.use('/.netlify/functions/api', router);
 module.exports.handler = serverless(app);
