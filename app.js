b373a2646216050cb984d5e8ab13876888bf7a42
RamaIlhamiSamda <ramasamda@gmail.com>
2024-07-19 18:23:21 +0700

    next commit

--- app.js
+++ app.js
@@ -7,53 +7,17 @@ const port = 3000
 let records = [];
 
 //Get all students
-router.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
 
 });
 
-//Create new record
-router.post('/add', (req, res) => {
-  res.send('New record added.');
-});
-
-//delete existing record
-router.delete('/', (req, res) => {
-  res.send('Deleted existing record');
-});
-
-//updating existing record
-router.put('/', (req, res) => {
-  res.send('Updating existing record');
-});
-
-//showing demo records
-router.get('/demo', (req, res) => {
-  res.json([
-    {
-      id: '001',
-      name: 'Smith',
-      email: 'smith@gmail.com',
-    },
-    {
-      id: '002',
-      name: 'Sam',
-      email: 'sam@gmail.com',
-    },
-    {
-      id: '003',
-      name: 'lily',
-      email: 'lily@gmail.com',
-    },
-  ]);
-});
 
 app.listen(port,()=>{
   console.log(`app is listening on port ${port}`)
 })
 
-app.use('/.netlify/functions/api', router);
-module.exports.handler = serverless(app);
+module.exports = app
 
 
 
--- functions/api.js
+++ functions/api.js
@@ -1,62 +1,24 @@
-const express = require('express');
-const serverless = require('serverless-http');
+const express = require("express");
+const serverless = require("serverless-http");
 const app = express();
 const router = express.Router();
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 
-var path = require('path')
+var path = require("path");
 
-let records = [];
-// app.use(express.static(__dirname + '/views/index.html'));
-app.use(bodyParser.json());
-
-//Get all students
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // res.sendFile(__dirname + '/views/index.html')
   // res.render('indexx.ejs');
   // res.send('bababa')
   // res.sendFile(path.join(__dirname, './index.html'));
   // res.sendFile(__dirname + './public/index.html')
-  res.sendFile(path.join(__dirname, './index.html'));
+  // res.sendFile(path.join(__dirname, '../index.html'));
+  router.get('/', (req, res) => {
+    // Assuming your HTML file is located in a folder named 'public'
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
 });
 
-//Create new record
-router.post('/add', (req, res) => {
-  res.send('New record added.');
-});
-
-//delete existing record
-router.delete('/', (req, res) => {
-  res.send('Deleted existing record');
-});
-
-//updating existing record
-router.put('/', (req, res) => {
-  res.send('Updating existing record');
-});
-
-//showing demo records
-router.get('/detail', (req, res) => {
-  res.json([
-    {
-      id: '001',
-      name: 'Smith',
-      email: 'smith@gmail.com',
-    },
-    {
-      id: '002',
-      name: 'Sam',
-      email: 'sam@gmail.com',
-    },
-    {
-      id: '003',
-      name: 'lily',
-      email: 'lily@gmail.com',
-    },
-  ]);
-});
-
-// app.use('/.netlify/functions/api', router);
-app.use('/',router)
-// app.use('/dada', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 module.exports.handler = serverless(app);
+
+
